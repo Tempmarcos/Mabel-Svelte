9075a089cf36bff5d7877e6d2ee9eb672ff852f9
Tempmarcos <marcostempdesouto@gmail.com>
2026-02-09 17:31:57 -0300

    first commit

---
+++ novaMabel/README.md
@@ -0,0 +1,6 @@
+
+# recreate this project
+npx sv create --template minimal --types ts --install npm novaMabel
+```
+
+Refazendo o frontend antigo com Svelte, dessa vez focando muito mais no UX e nas novas adições do backend.
---
+++ novaMabel/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+<main>
+    <div class="circulo"></div>
+</main>
+<style>
+    .circulo{
+        background-color: aqua;
+        height: 30px;
+        width: 30px;
+        
+    }
+</style>
